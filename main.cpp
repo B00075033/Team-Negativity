bfaf973dc9100a3d4ec693a0945600ebe5da5ffc
unknown <B00074381@E210-50015749.campus.itb.org>
2015-09-24 10:14:07 +0100

    Added Circle Shape, Changed Int's to Doubles

--- main.cpp
+++ main.cpp
@@ -1,44 +1,70 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 // Base class
-    class shape
+class shape
+{
+public:
+    //pure virtual function providing interface framework.
+    virtual double getArea() = 0;
+    void setWidth(double w)
     {
-    public:
-        //pure virtual function providing interface framework.
-        virtual int getArea() = 0;
-        void setWidth(int w)
-        {
-            width = w;
-        }
-        void setHeight(int h)
-        {
-            height = h;
-        }
-    protected:
-        int width;
-        int height;
-    };
+        width = w;
+    }
+    void setHeight(double h)
+    {
+        height = h;
+    }
+    void setRadius(double r)
+    {
+        radius = r;
+    }
+protected:
+    double width;
+    double height;
+    double radius;
+};
 
-    class Triangle: public shape
+class Triangle: public shape
+{
+public:
+    double getArea()
     {
-    public:
-        int getArea()
-        {
-            return (width * height)/2;
-        }
-    };
-class Rectangle: public Shape{public:   double getArea()   {      return (width * height);   }};
+        return (width * height)/2;
+    }
+};
+
+class Rectangle: public shape
+{
+public:
+    double getArea()
+    {
+        return (width * height);
+    }
+};
+class Circle: public shape
+{
+public:
+    double getArea()
+    {
+        return (3.14 * pow(radius,2));
+    }
+};
 int main(void)
 {
     Triangle Tri;
     Rectangle Rect;
+    Circle Cir;
     Tri.setWidth(5);
     Tri.setHeight(7);
-   Rect.setWidth(5);   Rect.setHeight(5);
+    Rect.setWidth(5);
+    Rect.setHeight(5);
+    Cir.setRadius(3);
     //Print area of the object.
     cout << "Total Triangle area : " << Tri.getArea()<< endl;
-   cout << "Total Rectangle area: " << Rect.getArea() << endl;    
-return 0;
+    cout << "Total Rectangle area: " << Rect.getArea() << endl;
+    cout << "Total Circle area: " << Cir.getArea() << endl;
+    return 0;
 }
